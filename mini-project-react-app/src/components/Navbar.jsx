032ba0660549af93ec3aa0b4beae07a38b1903e8
Thomas Kruithof <tkruithof95@gmail.com>
2024-06-30 13:15:43 +0200

    added logo, styled about page

--- mini-project-react-app/src/components/Navbar.jsx
+++ mini-project-react-app/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
+import Logo from "../assets/png/logo-1.png"
+
 
 const Navbar = () => {
     return (
         <nav className="nav-bar">
             <h3>App Name</h3>
-            <img src="" alt="app logo"/>
+            <img src={Logo} alt="app logo" className="app-logo"/>
         </nav>
     )
 }
